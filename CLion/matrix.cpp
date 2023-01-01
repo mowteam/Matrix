323aa04b553955b54fb474bc7333450cf67ff3db
mowteam <whrosenberg9@gmail.com>
2023-01-01 15:46:07 -0700

    fix while loop in progress

--- CLion/matrix.cpp
+++ CLion/matrix.cpp
@@ -4,22 +4,30 @@
 
 #include "matrix.h"
 #include <iostream>
-
+#include <string>
+#include <sstream>
 using namespace std;
 
 Matrix::Matrix(int row1, int col1): row(row1), col(col1), arr(NULL) 
 {
+    string line;
+    stringstream iss;
+
     arr = new double[row * col];
     for (int r = 0; r < row; r++) 
     {
         for (int c = 0; c < col; c++) 
         {
             double e;
-            cout << "Element (" << r << ", " << c << "): ";
-            while(!(cin >> e))
-            {
+            do{
+                iss.clear();
+                line = "";
                 cout << "Element (" << r << ", " << c << "): ";
+                getline(cin, line); 
+                iss << line;
             }
+            while(!(iss >> e));
+
             arr[toIndex(r, c)] = e;
         }
     }
