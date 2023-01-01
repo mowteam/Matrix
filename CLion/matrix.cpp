c658cf11d7ab72440443c274bfb35e61cf7baf36
mowteam <whrosenberg9@gmail.com>
2023-01-01 13:20:50 -0700

    example matrix class

---
+++ CLion/matrix.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Will Rosenberg on 1/1/23.
+//
+
+#include "matrix.h"
+
+Matrix::Matrix(unsigned int row, unsigned int col):row(row), col(col){}
+
+void Matrix::scalarMultiply(){
+    return;
+}
+
+
---
+++ CLion/matrix.h
@@ -0,0 +1,12 @@
+//
+// Created by Will Rosenberg on 1/1/23.
+//
+
+class Matrix{
+private:
+    unsigned int row;
+    unsigned int col;
+public:
+    Matrix(unsigned int row, unsigned int col);
+    void scalarMultiply();
+};
